a6f3226fa41bc2100b4467ef01875b2f2fb97c08
Long Ha Hoang <long@shenlong.de>
2023-04-24 14:39:23 +0200

    add: css grid to header, footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,17 +7,52 @@ export default function Header() {
       <h1>
         Long <span>Ha Hoang</span>
       </h1>
+      <div>
+        <p></p>
+      </div>
+      <div>
+        <Link href={"https://www.linkedin.com/in/long-ha-hoang-213018260/"}>
+          LinkedIn
+        </Link>
+        <Link href={"https://github.com/Long-HaHoang"}>Github</Link>
+        <Link href={"mailto://long@shenlong.de"}>Email</Link>
+      </div>
     </HomeHeader>
   );
 }
 
 const HomeHeader = styled.header`
+  padding: 1rem 4rem;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr;
+  gap: 0px 0px;
+  grid-template-areas: "headertext pages contact";
+
   h1 {
-    width: 100%;
+    grid-area: headertext;
+
     text-align: left;
+    font-weight: 400;
 
     @media (max-width: 991px) {
       text-align: center;
     }
+  }
+
+  h1 span {
+    font-weight: bold;
+  }
+
+  div {
+    grid-area: contact;
+
+    font-size: 1.2rem;
+    font-weight: bold;
+
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
   }
 `;
